77bd94e9f2a580a61f19fc9c670322bdd7e24000
Pericles Henrique <pericleshenriquelima@gmail.com>
2023-04-02 22:38:21 -0300

    Adcionei o componente Alert.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonicModule, ],
+  imports: [IonicModule,],
 })
 export class Tab1Page {
-  constructor() {}
+  constructor(public alertController: AlertController) { }
+  async exibirAlertaFavorito() {
+    const alert = await this.alertController.create({
+      header: 'Alerta!',
+      message: 'Deseja Realmente Favoritar o filme ?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'Cancel',
+          handler: (blah) => {
+            console.log('Confirm Cancel : blah');
+
+          }
+        }, {
+          text: 'SIM',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+
+
+
+
+
 }
