7b7ae61ebc17f33633d6455427e793e5c7b5aeaf
Pericles Henrique <pericleshenriquelima@gmail.com>
2023-04-02 23:01:30 -0300

    Adcionei ion-Toast para aparecer mensagem de confimação.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab1',
@@ -10,7 +12,7 @@ import { AlertController } from '@ionic/angular';
   imports: [IonicModule,],
 })
 export class Tab1Page {
-  constructor(public alertController: AlertController) { }
+  constructor(public alertController: AlertController, private  toastController: ToastController) { }
   async exibirAlertaFavorito() {
     const alert = await this.alertController.create({
       header: 'Alerta!',
@@ -20,13 +22,14 @@ export class Tab1Page {
           text: 'Cancelar',
           role: 'Cancel',
           handler: (blah) => {
-            console.log('Confirm Cancel : blah');
+            this.cancelarToast();
 
           }
         }, {
           text: 'SIM',
           handler: () => {
-            console.log('Confirm Okay');
+            this.apresentarToast();
+
           }
         }
       ]
@@ -35,8 +38,33 @@ export class Tab1Page {
   }
 
 
+async apresentarToast() {
+  const toast = await this.toastController.create({
+    message: ' Filme adcionado aos favoritos!',
+    duration: 2000,
+    color:'success',
 
+  });
 
-
-
+  toast.present();
 }
+
+
+async cancelarToast() {
+  const toast = await this.toastController.create({
+    message: ' cancelado',
+    duration: 2000,
+    color:'danger',
+
+  });
+
+  toast.present();
+}
+}
+
+
+
+
+
+
+
